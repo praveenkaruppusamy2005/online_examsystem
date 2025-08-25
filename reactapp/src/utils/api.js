9ad286813a86f700b12d2c7986940e306661d1ef
PRAVEEN KARUPPUSAMY M <150880557+praveenkaruppusamy2005@users.noreply.github.com>
2025-08-25 18:53:16 +0530

    Update api.js

--- reactapp/src/utils/api.js
+++ reactapp/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = 'onlineexamsystem-production.up.railway.app/api';
 
 // Create axios instance with default config
 const apiClient = axios.create({
