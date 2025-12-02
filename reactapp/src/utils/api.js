88c3d083f96b87da77b7c7deadef3c97edfd7033
PRAVEEN KARUPPUSAMY M <150880557+praveenkaruppusamy2005@users.noreply.github.com>
2025-12-02 17:10:13 +0530

    Update API URL to Render backend

--- reactapp/src/utils/api.js
+++ reactapp/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://onlineexamsystem-production.up.railway.app/api';
+const API_BASE = 'https://online-examsystem.onrender.com/api';
 
 // Create axios instance with default config
 const apiClient = axios.create({
